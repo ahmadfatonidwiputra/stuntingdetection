57e1364d652ea418523c2a11f2be9860c385aaa4
ahmadfatoni_dp <ahmadfatonidwiputra@Ahmads-MacBook-Air.local>
2026-03-31 14:21:40 +0800

    fix: add DATABASE_URL parser for Heroku PostgreSQL

--- config/database.php
+++ config/database.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Str;
 use Pdo\Mysql;
 
+// Parse DATABASE_URL dari Heroku
+$DATABASE_URL = env('DATABASE_URL') ? parse_url(env('DATABASE_URL')) : [];
+
 return [
 
     /*
@@ -17,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -86,17 +89,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'host' => $DATABASE_URL['host'] ?? env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'] ?? env('DB_PORT', '5432'),
+            'database' => ltrim($DATABASE_URL['path'] ?? env('DB_DATABASE', 'laravel'), '/'),
+            'username' => $DATABASE_URL['user'] ?? env('DB_USERNAME', 'root'),
+            'password' => $DATABASE_URL['pass'] ?? env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
